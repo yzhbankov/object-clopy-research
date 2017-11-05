0d317e9cc76fe837ff7d9565b939597b3d94560a
yzhbankov <yzhbankov@gmail.com>
2017-11-05 17:37:25 +0300

    render real data

--- client/components/BarChart.js
+++ client/components/BarChart.js
@@ -1,41 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {connect} from 'react-redux'
 import ReactHighcharts from 'react-highcharts'; // Expects that Highcharts was loaded in the code.
 
-const config = {
-    chart: {
-        renderTo: 'container',
-        type: 'bar'
-    },
-    title: {
-        text: 'Fruit Consumption'
-    },
-    xAxis: {
-        categories: ['Apples', 'Bananas', 'Oranges']
-    },
-    yAxis: {
-        title: {
-            text: 'Fruit eaten'
-        }
-    },
-    series: [{
-        name: 'Jane',
-        data: [1, 0, 4]
-    }, {
-        name: 'John',
-        data: [5, 7, 3]
-    }]
-};
 
-export default class BarChart extends React.Component {
+class BarChart extends React.Component {
     constructor(props) {
         super(props);
     }
-componentDidMount(){
-    ReactDOM.render(<ReactHighcharts config = {config} />, document.getElementById('chartId'));
-}
-    render() {
-    return <div id="chartId">
 
-    </div>}
+    componentWillReceiveProps() {
+        const objectDepths = [1, 2, 3, 5, 8, 13, 21, 34/*, 55, 89, 144, 233, 377, 610, 987*/];
+
+        const config = {
+            chart: {
+                renderTo: 'container',
+                type: 'bar'
+            },
+            title: {
+                text: 'Objects deep copy velocity'
+            },
+            xAxis: {
+                categories: objectDepths
+            },
+            yAxis: {
+                title: {
+                    text: 'Fruit eaten'
+                }
+            },
+            series: [{
+                name: 'JSON.parse',
+                data: this.props.parseData
+            }, {
+                name: 'clone',
+                data: this.props.cloneData
+            },
+                {
+                    name: 'deep copy',
+                    data: this.props.deepCopyData
+                },
+                {
+                    name: 'lodash',
+                    data: this.props.lodashData
+                }]
+        };
+        console.log('PROPERTIES', this.props);
+        ReactDOM.render(<ReactHighcharts config={config}/>, document.getElementById('chartId'));
+    }
+
+    render() {
+        return <div id="chartId">
+
+        </div>
+    }
 }
+
+
+const mapStateToProps = state => {
+    return {
+        parseData: state.parseData ? state.parseData : [],
+        cloneData: state.cloneData ? state.cloneData : [],
+        deepCopyData: state.deepCopyData ? state.deepCopyData : [],
+        lodashData: state.lodashData ? state.lodashData : []
+    }
+};
+
+export default connect(mapStateToProps)(BarChart);
